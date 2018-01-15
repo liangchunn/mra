459b5d9550d2e75f4fa4f82ac4c12b95c75ea77d
Liang Chun <liangchunn@me.com>
2018-01-15 16:56:14 +0100

    update sql

--- resources/groupdb.sql
+++ resources/groupdb.sql
@@ -2,9 +2,42 @@
 --- We need a new database that stores only group data,
 --- and one that actually stores which user is in which database
 
+-- CREATE TABLE GroupDatabase (
+--   id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+--   groupName VARCHAR(20) NOT NULL,
+--   adminId INT NOT NULL,
+--   memberIds VARCHAR(255) NOT NULL
+-- );
+--
+-- CREATE TABLE GroupMembers (
+--
+-- );
+
+CREATE TABLE Users (
+  userId INT AUTO_INCREMENT NOT NULL,
+  userName VARCHAR(20) NOT NULL,
+  PRIMARY KEY (userId),
+  UNIQUE (userName),
+);
+
 CREATE TABLE GroupDatabase (
-  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   groupName VARCHAR(20) NOT NULL,
   adminId INT NOT NULL,
-  memberIds VARCHAR(255) NOT NULL
+  PRIMARY KEY (groupName),
+  UNIQUE (groupName),
+  FOREIGN  KEY (adminId)
+    REFERENCES Users(userId)
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE GroupMembers (
+  groupName VARCHAR(20) NOT NULL,
+  memberId INT NOT NULL,
+  PRIMARY KEY (groupName),
+  FOREIGN KEY (groupName)
+    REFERENCES GroupDatabase(groupName)
+    ON UPDATE CASCADE,
+  FOREIGN KEY (memberId)
+    REFERENCES Users(userId)
+    ON UPDATE CASCADE
 );
